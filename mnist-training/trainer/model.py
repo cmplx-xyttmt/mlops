0433c89a485b02e3dcd0e0fbc9a58cb136d861d3
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-06-16 23:51:22 +0300

    Add flatten layer to neural network

--- mnist-training/trainer/model.py
+++ mnist-training/trainer/model.py
@@ -9,6 +9,7 @@ class NeuralNetwork(nn.Module):
 
     def __init__(self):
         super().__init__()
+        self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
                 nn.Linear(28 * 28 , 512),
                 nn.ReLU(),
